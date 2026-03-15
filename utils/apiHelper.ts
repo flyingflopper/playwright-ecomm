156e5dbf89760533c44624db98b300dfc975e50a
flyingflopper <nabilbinali00@gmail.com>
2026-03-15 18:58:35 +0800

    isloggedout

--- utils/apiHelper.ts
+++ utils/apiHelper.ts
@@ -4,10 +4,14 @@ export class ApiHelper {
   constructor(private request: APIRequestContext) {}
 
   async isLoggedIn(): Promise<boolean> {
-    const response = await this.request.get(
-      "https://ecommerce-playground.lambdatest.io/index.php?route=account/account"
-    );
+    const response = await this.request.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
     const body = await response.text();
     return response.ok() && body.includes("My Account") && !body.includes("account-login");
+  }
+
+  async isLoggedOut(): Promise<boolean> {
+    const response = await this.request.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/logout");
+    const body = await response.text();
+    return response.ok() && body.includes("Account Logout");
   }
 }
